5905295009fb810c654b172847af4c3c45f5d756
Stogi3 <30925584+StoGFly@users.noreply.github.com>
2022-04-04 10:14:39 +0800

    Update ScotlandPlace.user.js

--- ScotlandPlace.user.js
+++ ScotlandPlace.user.js
@@ -8,17 +8,17 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @grant        none
 // ==/UserScript==
+const overlay = "https://github.com/StoGFly/ScotlandPlace/new/main/rscotlandplaceoverlay.png?tstamp=" + Math.floor(Date.now() / 10000);
+
 if (window.top !== window.self) {
+    console.log("Placing overlay for r/Scotland:", overlay);
+
+    const overlay_img = document.createElement("img");
+    overlay_img.src = overlay;
+    overlay_img.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1999px;height: 1999px;";
+
     window.addEventListener('load', () => {
-            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
-        (function () {
-            const i = document.createElement("img");
-            i.src = "https://github.com/StoGFly/ScotlandPlace/new/main/rscotlandplaceoverlay.png?raw=true";
-            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1999px;height: 1999px;";
-            console.log(i);
-            return i;
-        })())
-
+        const canvas = document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0];
+        canvas.appendChild(overlay_img);
     }, false);
-
 }
