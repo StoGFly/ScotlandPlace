134b8482a84aa7f8017238384c36f07523377fb9
Stogi3 <30925584+StoGFly@users.noreply.github.com>
2022-04-04 10:27:23 +0800

    Update ScotlandPlace.user.js

--- ScotlandPlace.user.js
+++ ScotlandPlace.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         Scotland Logo template
+// @name         rScotland Logo template
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the canvas!
-// @author       oralekin
+// @version      0.11
+// @description  Save the Flag
+// @author       Stogie
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @grant        none
